e4f28fcf3c5d98086b875ae9552cf046d9261e1e
Nax <4265415+Nax@users.noreply.github.com>
2026-08-17 04:36:10 +0200

    Add support for client events

--- client/internal/app/app.go
+++ client/internal/app/app.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/OoTMM/multiplayer/client/internal/config"
+	"github.com/OoTMM/multiplayer/client/internal/events"
 	"github.com/OoTMM/multiplayer/client/internal/game"
 	"github.com/OoTMM/multiplayer/client/internal/ipc"
 	"github.com/OoTMM/multiplayer/shared/version"
@@ -15,9 +16,10 @@ import (
 )
 
 type App struct {
-	info *game.Info
-	conf *config.Config
-	ctx  context.Context
+	info   *game.Info
+	conf   *config.Config
+	ctx    context.Context
+	events *events.EventSink
 }
 
 func Run(ctx context.Context, conf *config.Config) error {
@@ -26,10 +28,17 @@ func Run(ctx context.Context, conf *config.Config) error {
 		return fmt.Errorf("failed to extract game info: %w", err)
 	}
 
+	events, err := events.NewEventSink(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to create event sink: %w", err)
+	}
+	defer events.Close()
+
 	app := &App{
-		info: info,
-		conf: conf,
-		ctx:  ctx,
+		info:   info,
+		conf:   conf,
+		ctx:    ctx,
+		events: events,
 	}
 
 	app.displayInfo()
